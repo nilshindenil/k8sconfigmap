f791bf9acd5d3bc5df15e4c1b84640812c7497d0
techsriman <tech.sriman@gmail.com>
2023-02-28 11:21:48 -0500

    fixed

--- corona/src/main/java/com/tripgo/controller/HealthInspectorController.java
+++ corona/src/main/java/com/tripgo/controller/HealthInspectorController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class HealthInspectorController {
+public class HealthInspectorController {
 	@Value("${config.location}")
 	private String configLocation;
 
